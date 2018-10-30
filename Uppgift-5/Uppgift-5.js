8a9bf5681927dd106edd4a2030fa6793621fa7ab
Andreas Åkerlöf <andreas@akerlof.se>
2018-10-30 15:38:36 +0100

    Nya kommentarer i Uppgift 5

--- Uppgift-5/Uppgift-5.js
+++ Uppgift-5/Uppgift-5.js
@@ -2,7 +2,7 @@
    JavaScript 1 - Inlämingsuppgift 3 - Uppgift 5
    Frekvenstabell över tärningskast - Scriptet genererar 1000 tärningskast
    och räknar ut hur många gånger varje siffra rullats.
-   Andreas Åkerlöf, 2018-10-18
+   Andreas Åkerlöf, 2018-10-30
 */
 
 // Funktionen randomDice rullar det antal tärningar som anges i rolls och returnerar resultaten i en array.
@@ -21,9 +21,9 @@ function randomDice(rolls) {
 
 function print() {
     let diceRolls = randomDice(1000);
-    let count = [0, 0, 0, 0, 0, 0];
+    let count = [0, 0, 0, 0, 0, 0]; // Array för att räkna antal. [1:or, 2:or, 3:or, 4:or, 5:or, 6:or]
     for (let value in diceRolls) {
-        count[diceRolls[value]-1]++;
+        count[diceRolls[value]-1]++; // Om det är 1:or ökar värdet med 1 i count[0], 2or i count[1] osv
     }
     console.log(`
         Frekvenstabell över tärningskast.
